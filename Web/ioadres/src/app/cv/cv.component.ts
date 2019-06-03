c12a41f17cc76c26550abe9303eb190003872d9b
Andres Garcia <andres.garcia@csccsl.com>
2019-06-03 09:46:13 +0200

    icons  to list

--- Web/ioadres/src/app/cv/cv.component.ts
+++ Web/ioadres/src/app/cv/cv.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
+const AssetPath = '/assets/'
 @Component({
   selector: 'cv',
   templateUrl: './cv.component.html',
   styleUrls: ['./cv.component.scss']
 })
-export class CvComponent {
-  title = 'CV';
-
+export class CvComponent  {
+  TechsRow1 : string[] = [`${AssetPath}angular.png`,`${AssetPath}vue.png`,`${AssetPath}netcore.jpg`,`${AssetPath}netframework.png`,`${AssetPath}xamarin.png`];
+  TechsRow2 : string[] = [`${AssetPath}git.png`,`${AssetPath}tfs.png`,`${AssetPath}c.png`,`${AssetPath}js.png`,`${AssetPath}sass.png`];
+  TechsRow3 : string[] = [`${AssetPath}mysql.jpg`,`${AssetPath}sqls.png`];
 }
